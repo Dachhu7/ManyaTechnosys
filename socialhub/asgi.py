5029598de01947f8724b31c87930b4dbbfedce4a
Darshan K ⑦ <kbdarshan1008@gmail.com>
2024-09-03 19:50:15 +0530

    Initial commit

--- socialhub/asgi.py
+++ socialhub/asgi.py
@@ -4,3 +4,4 @@ from django.core.asgi import get_asgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'socialhub.settings')
 
 application = get_asgi_application()
+git remote set-url origin https://github.com/Dachhu7/ManyaTechnosys.git
